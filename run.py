a9ce025131ec7762c88625e276bbc9b22db5f148
Connor (work) <connor.taylor@icloud.com>
2020-09-24 13:56:45 +1200

    first iter

---
+++ run.py
@@ -0,0 +1,125 @@
+import time
+import os 
+import argparse
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+
+import torchvision
+from torchvision import transforms
+
+from PIL import Image
+from collections import OrderedDict
+
+from train import 
+from models import VGG, GramMatrix, GramMSELoss
+
+if not os.path.exists('/images/'):
+    os.makedirs('/images/')
+
+if not os.path.exists('/outputs/'):
+    os.makedirs('/outputs/')
+
+if not os.path.exists('/models/'):
+    os.makedirs('/models/')
+
+image_dir = os.getcwd() + '/images/'
+model_dir = os.getcwd() + '/models/'
+
+def main():
+    parser = argparse.ArgumentParser(description='Style transfer')
+    parser.add_argument('--image', '-i', type=str, default=None, help='image path e.g. image.jpg')
+    parser.add_argument('--style', '-s', type=str, default=None, help='style path e.g. picasso.jpg')
+
+    args = parser.parse_args()
+
+    #
+    # Initialise
+    #
+
+    # ----------------- get data -----------------------------------------------------------
+    prep = prep_data(512)
+    postpa, postpb = post()
+
+    # ----------------- get model -----------------------------------------------------------
+    vgg = VGG()
+    vgg.load_state_dict(torch.load(model_dir + 'vgg_conv.pth'))
+    for param in vgg.parameters():
+        param.requires_grad = False
+    if torch.cuda.is_available():
+        vgg.cuda()
+    
+    # ----------------- load images -----------------------------------------------------------
+    img_dirs = [image_dir, image_dir]
+    img_names = [args.style, args.image]
+    imgs = [Image.open(img_dirs[i] + name) for i,name in enumerate(img_names)]
+    imgs_torch = [prep(img) for img in imgs]
+    if torch.cuda.is_available():
+        imgs_torch = [Variable(img.unsqueeze(0).cuda()) for img in imgs_torch]
+    else:
+        imgs_torch = [Variable(img.unsqueeze(0)) for img in imgs_torch]
+    style_image, content_image = imgs_torch
+
+    opt_img = Variable(content_image.data.clone(), requires_grad=True)
+
+    # ----------------- define layers -----------------------------------------------------------
+    style_layers = ['r11','r21','r31','r41', 'r51'] 
+    content_layers = ['r42']
+    loss_layers = style_layers + content_layers
+    loss_fns = [GramMSELoss()] * len(style_layers) + [nn.MSELoss()] * len(content_layers)
+    if torch.cuda.is_available():
+        loss_fns = [loss_fn.cuda() for loss_fn in loss_fns]
+        
+    #these are good weights settings:
+    style_weights = [1e3/n**2 for n in [64,128,256,512,512]]
+    content_weights = [1e0]
+    weights = style_weights + content_weights
+
+    #compute optimization targets
+    style_targets = [GramMatrix()(A).detach() for A in vgg(style_image, style_layers)]
+    content_targets = [A.detach() for A in vgg(content_image, content_layers)]
+    targets = style_targets + content_targets
+
+    #
+    # Low res
+    #
+
+    print("processing low res...")
+
+    out_img = train(opt_img, vgg, weights, loss_fns, targets, optim, loss_layers, postpa, postpb, low_res = True)
+
+    #
+    # high res
+    #
+
+    print("processing high res...")
+
+    #prep hr images
+    prep_hr = prep(800)
+    imgs_torch = [prep_hr(img) for img in imgs]
+    if torch.cuda.is_available():
+        imgs_torch = [Variable(img.unsqueeze(0).cuda()) for img in imgs_torch]
+    else:
+        imgs_torch = [Variable(img.unsqueeze(0)) for img in imgs_torch]
+    style_image, content_image = imgs_torch
+
+    #now initialise with upsampled lowres result
+    opt_img = prep_hr(out_img).unsqueeze(0)
+    opt_img = Variable(opt_img.type_as(content_image.data), requires_grad=True)
+
+    #compute hr targets
+    style_targets = [GramMatrix()(A).detach() for A in vgg(style_image, style_layers)]
+    content_targets = [A.detach() for A in vgg(content_image, content_layers)]
+    targets = style_targets + content_targets
+
+    out_img_hr = train(opt_img, vgg, weights, loss_fns, targets, optim, loss_layers, postpa, postpb, low_res = False)
+
+    out_img_hr.save(f'outputs/{str(img_names[1]).split(".")[0]}_{str(img_names[0]).split(".")[0]}_out_hr.jpg')
+    
+
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,65 @@
+import torch
+import torchvision
+from torchvision import transforms
+
+from matplotlib import pyplot
+from PIL import Image
+
+import os
+
+def prep_data(img_size):
+    prep = transforms.Compose([transforms.Resize(img_size),
+                            transforms.ToTensor(),
+                            transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
+                            transforms.Normalize(mean=[0.40760392, 0.45795686, 0.48501961], #subtract imagenet mean
+                                                    std=[1,1,1]),
+                            transforms.Lambda(lambda x: x.mul_(255)),
+                            ])
+    
+    return prep
+
+def post():
+    postpa = transforms.Compose([transforms.Lambda(lambda x: x.mul_(1./255)),
+                            transforms.Normalize(mean=[-0.40760392, -0.45795686, -0.48501961], #add imagenet mean
+                                                    std=[1,1,1]),
+                            transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to RGB
+                            ])
+    postpb = transforms.Compose([transforms.ToPILImage()])
+
+    return postpa, postpb
+
+def postp(tensor, postpa, postpb): # to clip results in the range [0,1]
+    t = postpa(tensor)
+    t[t>1] = 1    
+    t[t<0] = 0
+    img = postpb(t)
+    return img
+
+def train(opt_img, vgg, weights, loss_fns, targets, optim, loss_layers, postpa, postpb, low_res = True):
+    #run style transfer
+    max_iter = (500 if low_res else 200)
+    show_iter = 50
+    optimizer = optim.LBFGS([opt_img])
+    n_iter=[0]
+
+    while n_iter[0] <= max_iter:
+
+        def closure():
+            optimizer.zero_grad()
+            out = vgg(opt_img, loss_layers)
+            layer_losses = [weights[a] * loss_fns[a](A, targets[a]) for a,A in enumerate(out)]
+            loss = sum(layer_losses)
+            loss.backward()
+            n_iter[0]+=1
+            if n_iter[0]%show_iter == (show_iter-1):
+                print('Iteration: %d, loss: %f'%(n_iter[0]+1, loss.item()))
+            return loss
+        
+        optimizer.step(closure)
+        
+    #display result
+    out_img = postp(opt_img.data[0].cpu().squeeze(), postpa, postpb)
+    pyplot.imshow(out_img)
+    pyplot.gcf().set_size_inches(10,10)
+
+    return out_img
